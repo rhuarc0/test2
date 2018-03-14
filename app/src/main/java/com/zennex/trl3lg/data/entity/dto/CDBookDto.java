4779145f614d63d7ae29fef98cc49124df762f8b
Andrei <aosipov@zennex.ru>
2018-03-14 13:44:24 +0700

    Add - CDBook

--- app/src/main/java/com/zennex/trl3lg/data/entity/dto/CDBookDto.java
+++ app/src/main/java/com/zennex/trl3lg/data/entity/dto/CDBookDto.java
@@ -1,8 +1,41 @@
 package com.zennex.trl3lg.data.entity.dto;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by nikita on 14.03.18.
  */
 
-public class CDBookDto {
+public class CDBookDto extends RentalBookDto {
+
+    @SerializedName("ShippingDate")
+    private String mShippingDate;
+    @SerializedName("TrackingNumber")
+    private String mTrackingNumber;
+    @SerializedName("TrackingUrl")
+    private String mTrackingUrl;
+
+    public String getShippingDate() {
+        return mShippingDate;
+    }
+
+    public void setShippingDate(String shippingDate) {
+        mShippingDate = shippingDate;
+    }
+
+    public String getTrackingNumber() {
+        return mTrackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        mTrackingNumber = trackingNumber;
+    }
+
+    public String getTrackingUrl() {
+        return mTrackingUrl;
+    }
+
+    public void setTrackingUrl(String trackingUrl) {
+        mTrackingUrl = trackingUrl;
+    }
 }
--- app/src/main/java/com/zennex/trl3lg/data/mapper/dtomapper/CDBookDtoMapper.java
+++ app/src/main/java/com/zennex/trl3lg/data/mapper/dtomapper/CDBookDtoMapper.java
@@ -1,8 +1,29 @@
 package com.zennex.trl3lg.data.mapper.dtomapper;
 
+import android.support.annotation.NonNull;
+
+import com.annimon.stream.function.Supplier;
+import com.zennex.trl3lg.data.entity.dto.CDBookDto;
+import com.zennex.trl3lg.data.mapper.base.Mapper;
+import com.zennex.trl3lg.domain.entities.CDBook;
+
 /**
  * Created by nikita on 14.03.18.
  */
 
-public class CDBookDtoMapper {
+public class CDBookDtoMapper extends RentalBookDtoMapper<CDBookDto, CDBook> {
+
+    public CDBookDtoMapper() {
+        super(CDBook::new);
+    }
+
+    @NonNull
+    @Override
+    protected CDBook transform(@NonNull CDBookDto cdBookDto, CDBook cdBook) {
+        super.transform(cdBookDto, cdBook);
+        cdBook.setShippingDate(cdBookDto.getShippingDate());
+        cdBook.setTrackingNumber(cdBookDto.getTrackingNumber());
+        cdBook.setTrackingUrl(cdBookDto.getTrackingUrl());
+        return cdBook;
+    }
 }
